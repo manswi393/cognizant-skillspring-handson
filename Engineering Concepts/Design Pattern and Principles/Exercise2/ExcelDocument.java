3f441d7b10f070088d048bcd0758c4ab08f6395f
Manswi Ajankar <manswiajankar23@gmail.com>
2026-06-20 21:28:13 +0530

    first commit

---
+++ Engineering Concepts/Design Pattern and Principles/Exercise2/ExcelDocument.java
@@ -0,0 +1,10 @@
+package Exercise2;
+
+public class ExcelDocument {
+    
+}
+public class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document");
+    }
+}
