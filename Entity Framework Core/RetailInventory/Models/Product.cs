557f75b8db1c5f646761ac7756f2a4feb5b972bc
Manswi Ajankar <manswiajankar23@gmail.com>
2026-06-28 21:04:49 +0530

    algorithms data str inserted

---
+++ Entity Framework Core/RetailInventory/Models/Product.cs
@@ -0,0 +1,17 @@
+namespace RetailInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+
+        // Navigation Property
+        public Category Category { get; set; }
+    }
+}
