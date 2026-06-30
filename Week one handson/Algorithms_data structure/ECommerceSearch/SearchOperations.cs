f75e7f1ecca95e4a82c11b64ff2b3f1a8be58cba
Manswi Ajankar <manswiajankar23@gmail.com>
2026-06-30 19:44:23 +0530

    Moved folders into Week 1 Handson directory

---
+++ Week one handson/Algorithms_data structure/ECommerceSearch/SearchOperations.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace ECommerceSearch
+{
+    public class SearchOperations
+    {
+        // Linear Search
+        public static Product LinearSearch(Product[] products, int id)
+        {
+            foreach (Product product in products)
+            {
+                if (product.ProductId == id)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        // Binary Search
+        public static Product BinarySearch(Product[] products, int id)
+        {
+            int low = 0;
+            int high = products.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (products[mid].ProductId == id)
+                    return products[mid];
+
+                if (products[mid].ProductId < id)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+
+            return null;
+        }
+    }
+}
